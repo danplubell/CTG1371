5d98e5b6663a71d2a81403b3f9b16d05f9e85387
Dan Plubell <dplubell@cerner.com>
2015-01-26 14:54:17 -0600

    added tests for CTG Status block

--- test-suite/CTG1371Spec.hs
+++ test-suite/CTG1371Spec.hs
@@ -33,19 +33,76 @@ spec =
         it "DECG is disabled" $ do
             decgLogicOn (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
 
+        it "HR Cross Channel Verification is enabled" $ do
+            hrCrossChannelVer (ctgStatus ( ctgParser testDataHRCrossOn)) `shouldBe` True
+
+        it "HR Cross Channel Verification is disabled" $ do
+            hrCrossChannelVer (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+        it "Telemetry is enabled" $ do
+            telemetryOn (ctgStatus ( ctgParser testDataTelemetryOn)) `shouldBe` True
+
+        it "Telemetry is disabled" $ do
+            telemetryOn (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+        it "fspo2 is enabled" $ do
+            fspo2Available (ctgStatus ( ctgParser testDataFspo2Available)) `shouldBe` True
+
+        it "fspo2 is disabled" $ do
+            fspo2Available (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+        it "CTG data deleted is enabled" $ do
+            ctgDataDeleted (ctgStatus ( ctgParser testDataCTGDataDeletedOn)) `shouldBe` True
+
+        it "CTG data deleted is disabled" $ do
+            ctgDataDeleted (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+        it "CTG data inserted is enabled" $ do
+            ctgDataInserted (ctgStatus ( ctgParser testDataCTGDataInsertedOn)) `shouldBe` True
+
+        it "CTG data inserted is disabled" $ do
+            ctgDataInserted (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+        it "Monitor On is enabled" $ do
+            monitorOn (ctgStatus ( ctgParser testDataMonitorOn)) `shouldBe` True
+
+        it "Monitor On is disabled" $ do
+            monitorOn (ctgStatus ( ctgParser testDataAllOff)) `shouldBe` False
+
+
+testDataMonitorOn::ByteString
+testDataMonitorOn =  pack [0x43, 0x80 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+testDataCTGDataInsertedOn::ByteString
+testDataCTGDataInsertedOn =  pack [0x43, 0x40 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+testDataCTGDataDeletedOn::ByteString
+testDataCTGDataDeletedOn =  pack [0x43, 0x20 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+testDataFspo2Available::ByteString
+testDataFspo2Available =  pack [0x43, 0x8 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+testDataTelemetryOn::ByteString
+testDataTelemetryOn =  pack [0x43, 0x2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+testDataHRCrossOn::ByteString
+testDataHRCrossOn =  pack [0x43, 0x1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 testDataDECGOn::ByteString
-testDataDECGOn =  pack [67, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+testDataDECGOn =  pack [0x43, 0, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 testDataHrTwinOn::ByteString
-testDataHrTwinOn =  pack [67, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+testDataHrTwinOn =  pack [0x43, 0, 0x2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 testDataNoCBlock :: ByteString
-testDataNoCBlock = pack [66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+testDataNoCBlock = pack [0x42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
 
 testDataFMPEnabled::ByteString
-testDataFMPEnabled = pack [67, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+testDataFMPEnabled = pack [0x43, 0, 0x1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 testDataAllOff::ByteString
-testDataAllOff = pack [67, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+testDataAllOff = pack [0x43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
