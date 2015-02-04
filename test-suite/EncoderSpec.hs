d9c2b56a1f6a45b6306a47ddbcc7d408a4ad0871
Dan Plubell <dplubell@cerner.com>
2015-02-04 12:49:06 -0600

    added test for encoding the HR1 values

--- test-suite/EncoderSpec.hs
+++ test-suite/EncoderSpec.hs
@@ -5,6 +5,7 @@ import Data.CTG1371.Internal.Encoder.Encoders
 import Data.CTG1371.Internal.Types
 import Data.Word
 import Data.List (foldl')
+import qualified Data.ByteString as BS
 
 binToWord16 :: String -> Word16
 binToWord16 = foldl' (\acc x -> acc * 2 + digToWord16 x) 0
@@ -15,24 +16,31 @@ binToWord16 = foldl' (\acc x -> acc * 2 + digToWord16 x) 0
 
 spec ::Spec
 spec = describe "Test Encoder Internal Functions" $ do
-         it "packWord8 all true" $ do
+         it "test packWord8 all true" $ do
            packWord8 (True,True,True,True,True,True,True,True) `shouldBe` 0xFF
            
-         it "packWord8 all false" $ do
+         it "test packWord8 all false" $ do
            packWord8 (False,False,False,False,False,False,False,False) `shouldBe` 0
 
-         it "pack an HR for hr=100 fm == movement and Green signal quality" $ do
+         it "test encodeHR for hr=100 fm == movement and Green signal quality" $ do
            encodeHR 100 Movement SignalGreen `shouldBe` binToWord16 "0100100110010000"
 
-         it "pack an HR for hr = 256 fm = no movement and Red signal quality" $ do
+         it "test encodeHR hr = 256 fm = no movement and Red signal quality" $ do
            encodeHR 256 NoMovement SignalRed `shouldBe` binToWord16 "0000010000000000"
 
-         it "pack an HR for hr = 256 fm = movement and Yellow signal quality" $ do
+         it "test encodeHR  for hr = 256 fm = movement and Yellow signal quality" $ do
            encodeHR 256 Movement SignalYellow `shouldBe` binToWord16 "0010110000000000"
-           
---         it "packHR1 for heart rate of 100, NoFetalMovement, SignalGreen " $ do
---           packHR1 (buildHR1 Movement SignalGreen 100 False) [] `shouldBe` [0::Word8] 
 
+         it "test packHR1 for hr = 256 fm = movement and Yellow signal quality" $ do
+           packHR1 (buildHR1 Movement SignalYellow  256 False) [] `shouldBe` [44,0]
 
-
+         it "test packHR1 for hr = 100 fm = no movement and Red signal quality" $ do
+           packHR1 (buildHR1 NoMovement SignalRed 100 False) [] `shouldBe` [1,144]
                           
+         it "test encodeHR1 for the correct order of HR values" $ do
+           encodeHR1 [ ( buildHR1 NoMovement SignalRed 100 False)
+                      ,( buildHR1 NoMovement SignalRed 101 False)
+                      ,( buildHR1 NoMovement SignalRed 102 False)
+                      ,( buildHR1 NoMovement SignalRed 103 False)
+                     ] `shouldBe` BS.pack [1,156,1,152,1,148,1,144]
+         
